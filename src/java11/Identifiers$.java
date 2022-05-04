8918c29a07b34de5c27f01978443362827cea854
Gustav4060 <gustav4060eps@gmail.com>
2022-05-04 09:30:25 -0500

    prueba identifireds
    prueba identifireds

---
+++ src/java11/Identifiers$.java
@@ -0,0 +1,13 @@
+package java11;
+
+public class Identifiers$ {
+	public static void main(String[] args) {
+		String $ = "Almendra";
+		String $_ = "Paz";
+		String $_3 = "Rodriguez";
+		String DD454 = "Gustavo";
+		System.out.println("Valor $: " + $);
+		System.out.println("Valor$:" + $_);
+		System.out.println("Valor $_3: " + $_3);
+	}
+}
